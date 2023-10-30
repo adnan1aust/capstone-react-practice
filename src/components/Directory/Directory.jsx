3cab726ba4ff678e323f33ea9a06a41c7b98b466
Ahmed Shahriar Adnan <adnan1aust@gmail.com>
2023-10-30 15:40:04 +0600

    Added basic routing

--- src/components/Directory/Directory.jsx
+++ src/components/Directory/Directory.jsx
@@ -2,13 +2,13 @@ import "./Directory.scss";
 import CategoryItem from "../CategoryItem/CategoryItem";
 
 const Directory = ({ categories }) => {
-    return (
-        <div className="directory-container">
-            {categories.map((category) => (
-                <CategoryItem category={category} key={category.id} />
-            ))}
-        </div>
-    );
+  return (
+    <div className="directory-container">
+      {categories.map((category) => (
+        <CategoryItem category={category} key={category.id} />
+      ))}
+    </div>
+  );
 };
 
 export default Directory;
---
+++ src/routes/Navigation/Navigation.jsx
@@ -0,0 +1,26 @@
+import { Outlet, Link } from "react-router-dom";
+import "./Navigation.scss";
+import Logo from "../../assets/crown.svg";
+
+const Navigation = () => {
+  return (
+    <>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <img src={Logo} className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+          <Link className="nav-link" to="/signIn">
+            Sign In
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </>
+  );
+};
+
+export default Navigation;
