ba9d580c08a38a6a1e569f2a86a23557bd5bd991
Ahmed Shahriar Adnan <adnan1aust@gmail.com>
2023-10-30 10:00:31 +0600

    Initial commit

---
+++ src/components/Directory/Directory.jsx
@@ -0,0 +1,14 @@
+import "./Directory.scss";
+import CategoryItem from "../CategoryItem/CategoryItem";
+
+const Directory = ({ categories }) => {
+    return (
+        <div className="directory-container">
+            {categories.map((category) => (
+                <CategoryItem category={category} key={category.id} />
+            ))}
+        </div>
+    );
+};
+
+export default Directory;
